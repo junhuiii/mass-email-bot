0fd47707a54ddf3c29304732e503eded9fe073f3
xxfcbjhxx <aujunhui88@gmail.com>
2021-12-17 16:14:47 +0800

    Add TODO statements and new config.toml file

---
+++ src/main.py
@@ -0,0 +1,21 @@
+import pytoml
+import smtplib
+import os
+from email.message import EmailMessage
+
+# TODO: Function to import from config.toml, which contains environment variables
+
+# TODO: Function to create SMTP connection to gmail account (Use SSL Class)
+
+# TODO: Function to create connection to localhost for testing purposes
+
+# TODO: Function to log into gmail account
+
+# TODO: Function to open documents in attachments directory
+
+# TODO: Function to add attachment to email before sending
+
+# TODO: Function to get list of senders to send to from config.toml
+
+if __name__ == '__main__':
+    print("Hello World!")
