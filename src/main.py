babcdfa8a7d85054a0656a2e099e9516e4e6669d
xxfcbjhxx <aujunhui88@gmail.com>
2021-12-18 12:32:38 +0800

    Add debug_local_server function

--- src/main.py
+++ src/main.py
@@ -14,7 +14,17 @@ def read_config(path):
 
 # TODO: Function to create SMTP connection to gmail account (Use SSL Class)
 
+
 # TODO: Function to create connection to localhost for testing purposes
+# For windows: open cmd and input <py -m smtpd -c DebuggingServer -n localhost:1025>
+# For MacOS: open terminal and input <python3 -m smtpd -c DebuggingServer -n localhost:1025>
+# For testing purpose, EMAIL_ADDRESS AND receiver can be blank strings
+def debug_local_server(subject, body):
+    test_email = ""
+    receiver = ""
+    with smtplib.SMTP('localhost', 1025) as smtp:
+        msg = f'Subject: {subject}\n\n{body}'
+        smtp.sendmail(test_email, receiver, msg)
 
 # TODO: Function to log into gmail account
 
@@ -22,7 +32,7 @@ def read_config(path):
 
 # TODO: Function to add attachment to email before sending
 
-# TODO: Function to get list of senders to send to from config.toml
+# TODO: Function to get list of email receivers to send to from config.toml
 
 if __name__ == '__main__':
     print("Hello World!")
